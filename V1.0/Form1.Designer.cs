e5ba1d0b0b143dbc50a320a063b6a929282f877c
Thanushanthan <thanushanthan08@gmail.com>
2025-05-28 21:57:54 +0530

    Initial version

--- V1.0/Form1.Designer.cs
+++ V1.0/Form1.Designer.cs
@@ -34,15 +34,13 @@
             this.txtDueDate = new System.Windows.Forms.TextBox();
             this.lblDueDate = new System.Windows.Forms.Label();
             this.listBoxTasks = new System.Windows.Forms.ListBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(62, 217);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(113, 53);
             this.button1.TabIndex = 0;
@@ -54,7 +52,7 @@
             // 
             this.txtTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtTask.Location = new System.Drawing.Point(148, 50);
-            this.txtTask.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtTask.Margin = new System.Windows.Forms.Padding(4);
             this.txtTask.Name = "txtTask";
             this.txtTask.Size = new System.Drawing.Size(184, 36);
             this.txtTask.TabIndex = 1;
@@ -74,7 +72,7 @@
             // 
             this.txtDueDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtDueDate.Location = new System.Drawing.Point(148, 95);
-            this.txtDueDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtDueDate.Margin = new System.Windows.Forms.Padding(4);
             this.txtDueDate.Name = "txtDueDate";
             this.txtDueDate.Size = new System.Drawing.Size(184, 36);
             this.txtDueDate.TabIndex = 1;
@@ -96,49 +94,23 @@
             this.listBoxTasks.FormattingEnabled = true;
             this.listBoxTasks.ItemHeight = 25;
             this.listBoxTasks.Location = new System.Drawing.Point(341, 54);
-            this.listBoxTasks.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBoxTasks.Margin = new System.Windows.Forms.Padding(4);
             this.listBoxTasks.Name = "listBoxTasks";
             this.listBoxTasks.Size = new System.Drawing.Size(332, 129);
             this.listBoxTasks.TabIndex = 3;
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(247, 217);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(113, 53);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Clear";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(425, 217);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(113, 53);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "Delete";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(814, 394);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.listBoxTasks);
             this.Controls.Add(this.lblDueDate);
             this.Controls.Add(this.lblTask);
             this.Controls.Add(this.txtDueDate);
             this.Controls.Add(this.txtTask);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -154,8 +126,6 @@
         private System.Windows.Forms.TextBox txtDueDate;
         private System.Windows.Forms.Label lblDueDate;
         private System.Windows.Forms.ListBox listBoxTasks;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
     }
 }
 
